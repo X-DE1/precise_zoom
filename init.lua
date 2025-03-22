59628fe03ada0768d466e8955dc6ddf477ad2bf0
X-DE <101435795+X-DE1@users.noreply.github.com>
2025-03-21 20:03:12 -0600

    Zoom with Z in init.lua

--- init.lua
+++ init.lua
@@ -1,17 +1,54 @@
+local zoom_num = 10
+
 minetest.register_privilege("zoom", {
 	description = "Can zoom",
 	give_to_singleplayer = false
 })
 minetest.register_chatcommand("zoom", {
 	description = "Change the zoom with precision",
-	privs = {zoom = true,},
+	privs = {zoom = true},
 	func = function(name, param)
 		local player = minetest.get_player_by_name(name)
 		local param_num = tonumber(param)
-		if param_num == nil then
-			player:set_fov(0)
-		else
-			player:set_fov(param_num, false, 0.2)
+		if not (param_num == nil) then
+			zoom_num = param_num
 		end
 	end,
 })
+
+minetest.register_chatcommand("zoom", {
+	description = "Change the zoom with precision",
+	privs = {fly = true},
+	func = function(name, param)
+		local player = minetest.get_player_by_name(name)
+		local param_num = tonumber(param)
+		if not (param_num == nil) then
+			zoom_num = param_num
+		end
+	end,
+})
+
+minetest.register_chatcommand("zoom", {
+	description = "Change the zoom with precision",
+	privs = {creative = true},
+	func = function(name, param)
+		local player = minetest.get_player_by_name(name)
+		local param_num = tonumber(param)
+		if not (param_num == nil) then
+			zoom_num = param_num
+		end
+	end,
+})
+
+local function zoom()
+	for _,player in ipairs(minetest.get_connected_players()) do
+		if minetest.check_player_privs(player, {zoom = true}) or
+		minetest.check_player_privs(player, {fly = true}) or 
+		minetest.check_player_privs(player, {creative = true}) then
+			player:set_properties({zoom_fov = zoom_num})
+		end
+	end
+	minetest.after(1, zoom)
+end
+
+zoom()
